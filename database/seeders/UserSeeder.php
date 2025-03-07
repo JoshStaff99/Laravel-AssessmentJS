35d071ddc1f96382393ec7bc2a9b551e321b75b8
JoshStaff99 <Joshua.Stafford@netmatters-scs.com>
2025-03-07 12:33:54 +0000

    Admin user added
    Admin user added to the database and works as intended.

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\User;
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    public function run()
+    {
+        User::create([
+            'name' => 'Admin',
+            'email' => 'admin@admin.com',
+            'password' => bcrypt('password'),
+            'is_admin' => true,
+        ]);
+    }
+}
