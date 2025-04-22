c224df3de62464df571f3ad64b522685d9d572e7
JoshStaff99 <jstaffordmm10@gmail.com>
2025-04-22 12:16:52 +0100

    View listing updated
    Updated the views to use grid to have a more table style layout and to fill a lot of whitespace that was on the view pages. Added view, edit and delete buttons under each card in the employee and company listings view's

--- resources/views/companies/index.blade.php
+++ resources/views/companies/index.blade.php
@@ -3,19 +3,41 @@
         Companies Listings
     </x-slot:heading>
 
-    <div class="space-y-4">
+    <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
         @foreach ($companies as $company)
-            <a href="/companies/{{ $company->id }}" class="block px-4 py-6 border border-gray-200 rounded-lg hover:border-blue-800 group transition-colors duration-300">
+            <div class="block px-4 py-6 border border-gray-200 rounded-lg hover:border-blue-800 group transition-colors duration-300">
                 <div class="font-bold text-blue-500 text-sm">{{ $company->name }}</div>
 
                 <div>
                     <img class="text-laracasts" src="{{ asset($company->logo) }}" alt="Company Logo" />
                 </div>
-            </a>
+
+                
+                <div class="mt-6 flex items-center justify-between gap-x-6">
+                    <div>
+                        <x-button href="/companies/{{ $company->id }}">View Company Details</x-button> 
+                    </div>
+
+                    <div class="flex items-center gap-x-6">
+                        <p class="">
+                            <x-button href="/companies/{{ $company->id }}/edit">Edit</x-button> 
+                        </p>
+
+                        <div class="flex items-center">
+                            <button form="delete-form" class="text-red-500 text-sm font-bold">Delete</button>
+                        </div>
+                    </div>
+                </div>
+
+                <form method="POST" action="/companies/{{ $company->id }}" id="delete-form" class="hidden">
+                    @csrf
+                    @method('DELETE')
+                </form>
+            </div>
         @endforeach
 
-        <div>
-            {{ $companies->links() }}
-        </div>
+    </div>
+    <div class="mt-10">
+        {{ $companies->links() }}
     </div>
 </x-layout>
--- resources/views/employees/index.blade.php
+++ resources/views/employees/index.blade.php
@@ -3,19 +3,39 @@
         Employees Listings
     </x-slot:heading>
 
-    <div class="space-y-4">
+    <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
         @foreach ($employees as $employee)
-            <a href="/employees/{{ $employee->id }}" class="block px-4 py-6 border border-gray-200 rounded-lg hover:border-blue-800 group transition-colors duration-300">
+            <div class="block px-4 py-6 border border-gray-200 rounded-lg hover:border-blue-800 group transition-colors duration-300">
                 <div>
                     <h2 class="font-bold text-lg mb-5">Employee Name: {{ $employee->first_name }} {{ $employee->last_name }}</h2>
                     <h2 class="font-bold text-lg mb-5">Employee Email: {{ $employee->email }}</h2>
                     <h2 class="font-bold text-lg mb-5">Employee Phone Number: {{ $employee->phone_number }}</h2>
-                </div>
-            </a>
-        @endforeach
 
-        <div>
-            {{ $employees->links() }}
-        </div>
+                    <div class="mt-6 flex items-center justify-between gap-x-6">
+                    <div>
+                        <x-button href="/employees/{{ $employee->id }}">View Employee Details</x-button> 
+                    </div>
+
+                    <div class="flex items-center gap-x-6">
+                        <p class="">
+                            <x-button href="/employees/{{ $employee->id }}/edit">Edit</x-button> 
+                        </p>
+
+                        <div class="flex items-center">
+                            <button form="delete-form" class="text-red-500 text-sm font-bold">Delete</button>
+                        </div>
+                    </div>
+                </div>
+
+                <form method="POST" action="/employees/{{ $employee->id }}" id="delete-form" class="hidden">
+                    @csrf
+                    @method('DELETE')
+                </form>
+                </div>
+            </div>
+        @endforeach
+    </div>
+    <div class="mt-10">
+        {{ $employees->links() }}
     </div>
 </x-layout>
