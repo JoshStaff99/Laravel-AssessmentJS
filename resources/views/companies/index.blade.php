1de01a5e3e7100110a84fc01eca2fd7bee4bb72e
JoshStaff99 <jstaffordmm10@gmail.com>
2025-05-07 13:03:51 +0100

    Table layout fixes
    Table layout fixes and changes for companies view.

--- resources/views/companies/index.blade.php
+++ resources/views/companies/index.blade.php
@@ -3,13 +3,26 @@
         Companies Listings
     </x-slot:heading>
 
-    <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
+    <div class="hidden md:block min-h-full">
+            <div class="mx-auto max-w-7xl">
+                <div class="flex h-16 items-center bg-gray-800 border-y border-white">
+                    <h2 class="flex-1 text-white text-center px-3 py-2 text-sm font-medium">Company ID</h2>
+                    <h2 class="flex-1 text-white text-center px-3 py-2 text-sm font-medium">Company Logo</h2>
+                    <h2 class="flex-1 text-white text-center px-3 py-2 text-sm font-medium">Company Name</h2>
+                    <h2 class="flex-1 text-white text-center px-3 py-2 text-sm font-medium">Created At</h2>
+                </div>
+            </div>
+    </div>
+    <div class="grid grid-cols-1">
         @foreach ($companies as $company)
-            <div class="block px-4 py-6 border border-gray-200 rounded-lg hover:border-blue-800 group transition-colors duration-300">
-                <div class="font-bold text-blue-500 text-sm">{{ $company->name }}</div>
-
-                <div>
-                    <img class="text-laracasts" src="{{ asset($company->logo) }}" alt="Company Logo" />
+            <div class="block px-4 py-6 border border-gray-200 hover:border-blue-800 group transition-colors duration-300">
+                <div class="block md:flex items-center border-gray-300">
+                    <div class="flex-1 text-center px-3 py-2 text-sm text-blue-500 font-medium"><h2 class="md:hidden px-3">Company Id:</h2>{{ $company->id }}</div>
+                    <div class="flex-1 text-center px-3 py-2">
+                        <img src="{{ asset($company->logo) }}" alt="Company Logo" class="mx-auto h-20">
+                    </div>
+                    <div class="flex-1 text-center px-3 py-2 text-sm text-blue-500 font-medium"><h2 class="md:hidden px-3">Company Name:</h2>{{ $company->name }}</div>
+                    <div class="flex-1 text-center px-3 py-2 text-sm text-blue-500 font-medium"><h2 class="md:hidden px-3">Created At:</h2>{{ $company->created_at->format('Y-m-d') }}</div>
                 </div>
 
                 
--- resources/views/employees/index.blade.php
+++ resources/views/employees/index.blade.php
@@ -3,7 +3,7 @@
         Employees Listings
     </x-slot:heading>
 
-    <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
+    <div class="grid grid-cols-1 gap-6">
         @foreach ($employees as $employee)
             <div class="block px-4 py-6 border border-gray-200 rounded-lg hover:border-blue-800 group transition-colors duration-300">
                 <div>
